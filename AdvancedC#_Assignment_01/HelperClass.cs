3bda34f55677c443d4f5e200c93f34b1784fc3b5
135ahmedsayed <ahmedshkra246@gmail.com>
2025-07-26 14:45:58 +0300

    Solve Q2

--- AdvancedC#_Assignment_01/HelperClass.cs
+++ AdvancedC#_Assignment_01/HelperClass.cs
@@ -26,3 +26,36 @@ public static class BubbleSort
     }
 }
 #endregion
+
+#region Q2
+public class Range<T>
+    where T : IComparable<T>
+{
+    public T? Minimum { get; set; }
+    public T? Maximum { get; set; }
+    public T? difference { get; set; }
+
+    //constructor
+    public Range(T min, T max)
+    {
+        Minimum = min;
+        Maximum = max;
+    }
+    //check if the value is in range
+    public bool IsInRange(T value)
+    {
+        if (value is null)
+            throw new ArgumentNullException(nameof(value));
+        Console.Write($"Is {value} is within the range ? ");
+        return value.CompareTo(Minimum) >= 0 && value.CompareTo(Maximum) <= 0;
+    }
+    //calculate the length of the range
+    public void Length()
+    {
+        if (Minimum is null && Maximum is null)
+            Console.WriteLine("Minmum And Maxmum are Null");
+        difference = (dynamic)Maximum - (dynamic)Minimum;
+        Console.WriteLine($"The Length of the Range is : {Maximum} - {Minimum} = {difference}");
+    }
+}
+#endregion
--- AdvancedC#_Assignment_01/Program.cs
+++ AdvancedC#_Assignment_01/Program.cs
@@ -13,6 +13,13 @@
             BubbleSort.Result(Numbers);
             Console.WriteLine("\n");
             #endregion
+
+            #region Q2
+            Range<double> range = new Range<double>(90.20, 99.22);
+            Console.WriteLine($"Maximum = {range.Maximum} && Minimum = {range.Minimum}");
+            Console.WriteLine(range.IsInRange(91));
+            range.Length();
+            #endregion
             Console.ReadKey();
         }
     }
