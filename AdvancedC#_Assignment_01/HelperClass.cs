223f0daa427a071db6b28a1d2f7d757d3f40ec08
135ahmedsayed <ahmedshkra246@gmail.com>
2025-07-26 14:44:18 +0300

    Solve Q1

--- AdvancedC#_Assignment_01/HelperClass.cs
+++ AdvancedC#_Assignment_01/HelperClass.cs
@@ -1,2 +1,28 @@
 ﻿namespace AdvancedC__Assignment_01;
-
+#region Q1
+public static class BubbleSort
+{
+    public static void BubbleSortAlgorithm<T>(T[] arr)
+        where T : IComparable<T>
+    {
+        if (arr is null)
+            throw new ArgumentNullException(nameof(arr));
+        T other;
+        for (int i = 0; i < arr.Length - 1; i++)
+            for (int j = 0; j < arr.Length - i - 1; j++)
+                if (arr[j].CompareTo(arr[j + 1]) == 1)
+                {
+                    other = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = other;
+                }
+    }
+    public static void Result<T>(T[] values)
+    {
+        foreach (var item in values)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
+#endregion
--- AdvancedC#_Assignment_01/Program.cs
+++ AdvancedC#_Assignment_01/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            int[] Numbers = { 21, 10, 1, 2, 3, 12, 90, 9 };
+            Console.Write("Before Sorting : ");
+            BubbleSort.Result(Numbers);
+            Console.Write("\nAfter Sorting : ");
+            BubbleSort.BubbleSortAlgorithm(Numbers);
+            BubbleSort.Result(Numbers);
+            Console.WriteLine("\n");
+            #endregion
             Console.ReadKey();
         }
     }
